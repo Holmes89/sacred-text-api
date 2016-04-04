72fe138ce68703c529278929577d22aa43947df1
Joel Holmes <Holmes89@gmail.com>
2016-04-03 21:32:36 -0400

    Quran Factory created

---
+++ src/test/java/com/ferrumlabs/factories/QuranFactoryTests.java
@@ -0,0 +1,75 @@
+package com.ferrumlabs.factories;
+
+import java.util.Map;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.ferrumlabs.SacredTextApiApplication;
+import com.ferrumlabs.enums.QuranVersionEnum;
+import com.ferrumlabs.exceptions.FactoryException;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = SacredTextApiApplication.class)
+public class QuranFactoryTests {
+
+	@Autowired
+	QuranFactory quranResource;
+	
+	@Test
+	public void testGetVerse() throws FactoryException{
+		String content = quranResource.getVerse(QuranVersionEnum.PICKTHALL, 1, 1);
+		Assert.assertEquals("In the name of Allah, the Beneficent, the Merciful.", content);
+	}
+	
+	@Test(expected=FactoryException.class)
+	public void testGetVerse_nullVersion() throws FactoryException{
+		quranResource.getVerse(null, 1, 1);
+	}
+	
+	@Test(expected=FactoryException.class)
+	public void testGetVerse_invalidChapter() throws FactoryException{
+		quranResource.getVerse(QuranVersionEnum.PICKTHALL, -1, 1);
+	}
+	
+	@Test(expected=FactoryException.class)
+	public void testGetVerse_invalidVerse() throws FactoryException{
+		quranResource.getVerse(QuranVersionEnum.PICKTHALL, 1, -1);
+	}
+	
+	@Test
+	public void testGetChapterVerses() throws FactoryException{
+		Map<Integer, String> verses = quranResource.getVerses(QuranVersionEnum.PICKTHALL, 1);
+		Assert.assertEquals(7, verses.size());
+	}
+	
+	@Test(expected=FactoryException.class)
+	public void testGetChapterVerses_nullVersion() throws FactoryException{
+		quranResource.getVerses(null, 1);
+	}
+	
+	@Test(expected=FactoryException.class)
+	public void testGetChapterVerses_invalidChapter() throws FactoryException{
+		quranResource.getVerses(QuranVersionEnum.PICKTHALL, -1);
+	}
+	
+	@Test
+	public void testGetChapterName() throws FactoryException{
+		String name = quranResource.getChapterName(QuranVersionEnum.PICKTHALL, 2);
+		Assert.assertEquals("The Cow", name);
+	}
+	
+	@Test(expected=FactoryException.class)
+	public void testGetChapterName_nullVersion() throws FactoryException{
+		quranResource.getChapterName(null, 1);
+	}
+	
+	@Test(expected=FactoryException.class)
+	public void testGetChapterName_invalidChapter() throws FactoryException{
+		quranResource.getChapterName(QuranVersionEnum.PICKTHALL, -1);
+	}
+}
