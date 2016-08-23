2832138e67c803a5f3da2fdfa01e748f7458b35e
Joel Holmes <holmes89@gmail.com>
2016-08-23 15:06:25 -0400

    search commands

---
+++ src/main/java/com/joeldholmes/commands/SearchQuranTextCommand.java
@@ -0,0 +1,52 @@
+package com.joeldholmes.commands;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+import com.joeldholmes.dto.VerseDTO;
+import com.joeldholmes.enums.BibleVersionEnum;
+import com.joeldholmes.exceptions.ServiceException;
+import com.joeldholmes.services.interfaces.ISearchService;
+import com.netflix.hystrix.HystrixCommandGroupKey;
+import com.netflix.hystrix.HystrixCommandKey;
+import com.netflix.hystrix.exception.HystrixBadRequestException;
+
+@Component
+@Scope("prototype")
+public class SearchQuranTextCommand extends BaseCommand<List<VerseDTO>> {
+	
+	@Autowired
+	ISearchService searchService;
+	
+	private String term;
+	
+	protected SearchQuranTextCommand() {
+		super(Setter.withGroupKey(HystrixCommandGroupKey.Factory.asKey("SacredTextAPI"))
+				.andCommandKey(HystrixCommandKey.Factory.asKey("SearchQuranText")));
+	}
+	
+	
+	public SearchQuranTextCommand setTerm(String term){
+		this.term=term;
+		return this;
+	}
+	
+		
+	@Override
+	protected List<VerseDTO> run() throws Exception {
+		try{
+			return searchService.searchQuranText(term);
+		}
+		catch(ServiceException e){
+			log.error("error creating searching "+e);
+			throw new HystrixBadRequestException("unable to process request", e);
+		}
+	}
+	
+	
+
+}
+
---
+++ src/main/java/com/joeldholmes/commands/SearchTaoTextCommand.java
@@ -0,0 +1,51 @@
+package com.joeldholmes.commands;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+import com.joeldholmes.dto.VerseDTO;
+import com.joeldholmes.exceptions.ServiceException;
+import com.joeldholmes.services.interfaces.ISearchService;
+import com.netflix.hystrix.HystrixCommandGroupKey;
+import com.netflix.hystrix.HystrixCommandKey;
+import com.netflix.hystrix.exception.HystrixBadRequestException;
+
+@Component
+@Scope("prototype")
+public class SearchTaoTextCommand extends BaseCommand<List<VerseDTO>> {
+	
+	@Autowired
+	ISearchService searchService;
+	
+	private String term;
+	
+	protected SearchTaoTextCommand() {
+		super(Setter.withGroupKey(HystrixCommandGroupKey.Factory.asKey("SacredTextAPI"))
+				.andCommandKey(HystrixCommandKey.Factory.asKey("SearchTaoText")));
+	}
+	
+	
+	public SearchTaoTextCommand setTerm(String term){
+		this.term=term;
+		return this;
+	}
+	
+		
+	@Override
+	protected List<VerseDTO> run() throws Exception {
+		try{
+			return searchService.searchTaoText(term);
+		}
+		catch(ServiceException e){
+			log.error("error creating searching "+e);
+			throw new HystrixBadRequestException("unable to process request", e);
+		}
+	}
+	
+	
+
+}
+
